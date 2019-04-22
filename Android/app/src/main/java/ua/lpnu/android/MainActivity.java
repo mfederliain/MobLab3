9db11c791a00894c065b79ccfe36da5de1c6881b
Serhii Kolisnyk <kolisnyk.sk@gmail.com>
2019-04-22 21:51:18 +0300

    do my task

--- Android/app/src/main/java/ua/lpnu/android/MainActivity.java
+++ Android/app/src/main/java/ua/lpnu/android/MainActivity.java
@@ -1,60 +1,157 @@
 package ua.lpnu.android;
 
 import android.content.Context;
-import android.content.DialogInterface;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.view.animation.AlphaAnimation;
+import android.view.ViewParent;
 import android.view.animation.Animation;
 import android.view.animation.TranslateAnimation;
+import android.widget.CheckBox;
+import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
-import java.util.Vector;
+import java.util.ArrayList;
 
-public class MainActivity extends AppCompatActivity implements View.OnClickListener
-{
+import ua.lpnu.android.pojo.Contact;
 
-    ViewGroup m_my_list;
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
+    ViewGroup viewGroup;
+    ArrayList<Contact> contactsChecked = new ArrayList<>();
+    ArrayList<Contact> contacts;
+    EditText phoneCall;
     @Override
-    protected void onCreate(Bundle savedInstanceState)
-    {
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        m_my_list = (ViewGroup)findViewById(R.id.list);
+        viewGroup = (ViewGroup) findViewById(R.id.list);
 
-        LayoutInflater l = (LayoutInflater) getSystemService(Context.LAYOUT_INFLATER_SERVICE);
 
-        Vector<String> strings = new Vector<String>();
-        for(int i=0;i<100;++i)
-        {
-            strings.add("Item " + i);
-        }
 
-        for(String s : strings)
-        {
-            View item = l.inflate( R.layout.list_item, null);
-            ((TextView)item.findViewById(R.id.itemtext)).setText(s);
-            item.findViewById(R.id.morebutton).setOnClickListener(this);
-            m_my_list.addView(item);
-        }
+        contacts = getContacs();
+
+        showData();
+        phoneCall = findViewById(R.id.phoneCall);
+
 
         findViewById(R.id.detail).setOnClickListener(this);
-        findViewById(R.id.button1).setOnClickListener(this);
-        findViewById(R.id.button2).setOnClickListener(this);
-        findViewById(R.id.button3).setOnClickListener(this);
+        findViewById(R.id.delete).setOnClickListener(this);
+        findViewById(R.id.edite).setOnClickListener(this);
+        findViewById(R.id.add).setOnClickListener(this);
+        findViewById(R.id.call).setOnClickListener(this);
+
+    }
+    public void showData(){
+        viewGroup.removeAllViews();
+        for (Contact c : contacts) {
+            //init items
+
+            LayoutInflater l = (LayoutInflater) getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+            final View item = l.inflate(R.layout.list_item, null);
+            ((TextView) item.findViewById(R.id.personis)).setText(String.valueOf(c.getIdPerson()));
+            ((CheckBox) item.findViewById(R.id.checkBox)).setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    if (v.getId() == R.id.checkBox) {
+                        CheckBox ch = (CheckBox) v;
+                        if (ch.isChecked()) {
+                            //contactsChecked.add(TextView item.findViewById(R.id.itemtext).);
+                            int idPerson = Integer.valueOf(((TextView) item.findViewById(R.id.personis)).getText().toString());
+                            Contact contact = findByIdPerson(idPerson);
+                            if (contact == null) {
+                                ch.setChecked(false);
+                                return;
+                            }
+                            Toast.makeText(getApplicationContext(),
+                                    "checked " + contact.getName(),
+                                    Toast.LENGTH_SHORT).show();
+                            contactsChecked.add(contact);
+                        } else {
+                            int idPerson = Integer.valueOf(((TextView) item.findViewById(R.id.personis)).getText().toString());
+                            Contact contact = findByIdPerson(idPerson);
+                            if (contact == null) {
+                                ch.setChecked(false);
+                                return;
+                            }
+                            Toast.makeText(getApplicationContext(),
+                                    "unchecked " + contact.getName(),
+                                    Toast.LENGTH_SHORT).show();
+                            contactsChecked.remove(contact);
+                        }
+                    }
+                }
+            });
+
+            ((TextView) item.findViewById(R.id.itemtext)).setText(c.getName());
+            ((TextView) item.findViewById(R.id.phoneNumber)).setText(c.getPhone());
+            item.findViewById(R.id.morebutton).setOnClickListener(this);
+            viewGroup.addView(item);
+        }
     }
 
-    public void onClick(View v)
-    {
-        if (v.getId()==R.id.morebutton)
-        {
+    public Contact findByIdPerson(int idPerson) {
+        for (Contact contact : contacts) {
+            if (contact.getIdPerson() == idPerson)
+                return contact;
+        }
+        return null;
+    }
+
+    public ArrayList<Contact> getContacs() {
+        ArrayList<Contact> contacts = new ArrayList<Contact>();
+        int count = 1;
+        contacts.add(new Contact(count, count, "Petro", "Poroshenko", "+38032321", Contact.TypePhone.FRIEND));
+        count++;
+        contacts.add(new Contact(count, count, "Vova", "Thrach", "+35645654", Contact.TypePhone.WORK));
+        count++;
+        contacts.add(new Contact(count, count, "My love", "Secret", "+38032342", Contact.TypePhone.FAMILY));
+        count++;
+        contacts.add(new Contact(count, count, "Yra", "Terasenko", "+38094534", Contact.TypePhone.HOME));
+        count++;
+        contacts.add(new Contact(count, count, "Andriy", "Kor", "+38094534", Contact.TypePhone.UNKNOW));
+
+        return contacts;
+    }
+
+    public void onClick(View v) {
+        if (v.getId() == R.id.morebutton) {
             View detailview = findViewById(R.id.detail);
+            View viewParent=v.getRootView();
+            int idPerson = Integer.valueOf(((TextView) viewParent.findViewById(R.id.personis)).getText().toString());
+            Contact c=findByIdPerson(idPerson);
+            ((TextView)detailview.findViewById(R.id.contactSurname)).setText(c.getSurname());
+            ((TextView)detailview.findViewById(R.id.contactName)).setText(c.getName());
+            ((TextView)detailview.findViewById(R.id.contactNumber)).setText(c.getPhone());
+            ImageView img=((ImageView)detailview.findViewById(R.id.contactImg));
+            switch (c.getTypePhone()){
+                case Contact.TypePhone
+                        .FAMILY:
+                    img.setImageDrawable(ContextCompat.getDrawable(getApplicationContext(),R.drawable.ic_favorite_border_black_24dp));
+                    break;
+                case Contact.TypePhone
+                        .FRIEND:
+                    img.setImageDrawable(ContextCompat.getDrawable(getApplicationContext(),R.drawable.ic_person_outline_black_24dp));
+
+                    break;
+                case Contact.TypePhone
+                        .HOME:
+                    img.setImageDrawable(ContextCompat.getDrawable(getApplicationContext(),R.drawable.ic_home_black_24dp));
+
+                    break;
+                case Contact.TypePhone
+                        .WORK:
+                    img.setImageDrawable(ContextCompat.getDrawable(getApplicationContext(),R.drawable.ic_airplay_black_24dp));
+
+                    break;
+            }
+
             float width = findViewById(R.id.main_layout).getWidth();
             TranslateAnimation anim = new TranslateAnimation(width, 0.0f, 0.0f, 0.0f);
             anim.setDuration(300);
@@ -63,14 +160,15 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
             detailview.startAnimation(anim);
             detailview.setVisibility(View.VISIBLE);
             detailview.setEnabled(true);
-        }
-        else if (v.getId()==R.id.detail)
-        {
+        } else if (v.getId() == R.id.detail) {
             View detailview = v;
+            //View parent=detailview.getRootView();
+
             TranslateAnimation anim = new TranslateAnimation(0.0f, detailview.getWidth(), 0.0f, 0.0f);
             anim.setAnimationListener(new Animation.AnimationListener() {
                 @Override
-                public void onAnimationStart(Animation animation) {}
+                public void onAnimationStart(Animation animation) {
+                }
 
                 @Override
                 public void onAnimationEnd(Animation animation) {
@@ -78,30 +176,47 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                 }
 
                 @Override
-                public void onAnimationRepeat(Animation animation) {}
+                public void onAnimationRepeat(Animation animation) {
+                }
             });
             anim.setDuration(300);
             anim.setFillAfter(true);
             detailview.startAnimation(anim);
             detailview.setEnabled(false);
-        }
-        else if (v.getId()==R.id.button1)
-        {
+        } else if (v.getId() == R.id.delete) {
             Toast.makeText(getApplicationContext(),
-                    "You clicked button 1",
+                    "Delete ...",
                     Toast.LENGTH_SHORT).show();
-        }
-        else if (v.getId()==R.id.button2)
-        {
+            for (int i = 0; i < contactsChecked.size(); i++) {
+                for (int j = 0; j < contacts.size(); j++) {
+                    if (contacts.get(j).getIdPerson()==contactsChecked.get(i).getIdPerson()){
+                        contacts.remove(j);
+                        break;
+                    }
+                }
+            }
+            contactsChecked.clear();
+            showData();
+
+        } else if (v.getId() == R.id.edite) {
             Toast.makeText(getApplicationContext(),
-                    "You clicked button 2",
+                    "Edit .... not work ^)",
                     Toast.LENGTH_SHORT).show();
-        }
-        else if (v.getId()==R.id.button3)
-        {
+        } else if (v.getId() == R.id.add) {
             Toast.makeText(getApplicationContext(),
-                    "You clicked button 3",
+                    "We add ....",
                     Toast.LENGTH_SHORT).show();
+            contacts.addAll(contactsChecked);
+            contactsChecked.clear();
+            showData();
+        } else if (v.getId() == R.id.call) {
+            String text=phoneCall.getText().toString();
+            if (text!=null && text.length()>0){
+                Toast.makeText(getApplicationContext(),
+                        "Calling "+text,
+                        Toast.LENGTH_SHORT).show();
+            }
+
         }
     }
 }
---
+++ Android/app/src/main/java/ua/lpnu/android/pojo/Contact.java
@@ -0,0 +1,107 @@
+package ua.lpnu.android.pojo;
+
+import android.support.annotation.IntDef;
+
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+
+import static ua.lpnu.android.pojo.Contact.TypePhone.FAMILY;
+import static ua.lpnu.android.pojo.Contact.TypePhone.FRIEND;
+import static ua.lpnu.android.pojo.Contact.TypePhone.HOME;
+import static ua.lpnu.android.pojo.Contact.TypePhone.UNKNOW;
+import static ua.lpnu.android.pojo.Contact.TypePhone.WORK;
+
+public class Contact {
+    int idPerson;
+    int idPhone;
+    private String Name;
+    private String Surname;
+    private String phone;
+
+    @TypePhone
+    private int typePhone;
+
+    public Contact(int idPerson, int idPhone, String name, String surname, String phone, int typePhone) {
+        this.idPerson = idPerson;
+        this.idPhone = idPhone;
+        Name = name;
+        Surname = surname;
+        this.phone = phone;
+        this.typePhone = typePhone;
+    }
+
+    public Contact(String name, String surname, String phone, @TypePhone int typePhone) {
+        Name = name;
+        Surname = surname;
+        this.phone = phone;
+        this.typePhone = typePhone;
+    }
+
+    public String getName() {
+        return Name;
+    }
+
+    public String getSurname() {
+        return Surname;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public int getTypePhone() {
+        return typePhone;
+    }
+
+    public int getIdPerson() {
+        return idPerson;
+    }
+
+    public void setIdPerson(int idPerson) {
+        this.idPerson = idPerson;
+    }
+
+    public int getIdPhone() {
+        return idPhone;
+    }
+
+    public void setIdPhone(int idPhone) {
+        this.idPhone = idPhone;
+    }
+
+    @IntDef({HOME,WORK,FAMILY,FRIEND,UNKNOW })
+    @Retention(RetentionPolicy.SOURCE)
+    public @interface TypePhone{
+        int HOME=1;
+        int WORK=2;
+        int FAMILY=3;
+        int FRIEND=4;
+        int UNKNOW=0;
+    };
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Contact)) return false;
+
+        Contact contact = (Contact) o;
+
+        if (idPerson != contact.idPerson) return false;
+        if (getTypePhone() != contact.getTypePhone()) return false;
+        if (getName() != null ? !getName().equals(contact.getName()) : contact.getName() != null)
+            return false;
+        if (getSurname() != null ? !getSurname().equals(contact.getSurname()) : contact.getSurname() != null)
+            return false;
+        return getPhone() != null ? getPhone().equals(contact.getPhone()) : contact.getPhone() == null;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = idPerson;
+        result = 31 * result + (getName() != null ? getName().hashCode() : 0);
+        result = 31 * result + (getSurname() != null ? getSurname().hashCode() : 0);
+        result = 31 * result + (getPhone() != null ? getPhone().hashCode() : 0);
+        result = 31 * result + getTypePhone();
+        return result;
+    }
+}
